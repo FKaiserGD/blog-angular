817ea86e19aa25a9f83616dadf897b8d574d1adf
Filipe Lacerda <gdfkaiser@gmail.com>
2023-12-22 06:42:37 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+    {
+        "id": 1,
+        "photo": "aaaa",
+        "title": "WONKA É SUCESSO NO MUNDO TODO",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eleifend placerat ante eu ornare. Nullam magna nisi, accumsan a euismod nec, fringilla vel mi.",
+        "photoCover": "https://play-lh.googleusercontent.com/_T9Vt0VnGN1xjjUEhw63nF2tI0RljQXx_XKwlJwfPmaBCLQ168CkO1ZUkkMGcgrbjHuCy7-4WKGf=w1296-h2160-rw",
+    },
+    {
+        "id": 2,
+        "photo": "aaaa",
+        "title": "noticia 2",
+        "description": "oi",
+        "photoCover": "",
+    }
+]
